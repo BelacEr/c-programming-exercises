6a3b10caa5fbfa48f26e3e7cc26f7e2f269fc38d
BelacEr <caleblopezmorgado2007@gmail.com>
2025-09-26 19:51:16 -0600

    chapter2

---
+++ 02_c_fundamentals/exercises/section_2_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>  // (a) directive 
+
+int main(void)
+{
+  printf("Parkinson's Law:\nWork expand so as to ");      // (a) statement
+  printf("fill the time\n");                              // (a) statement
+  printf("available for its completion.\n");              // (a) statement
+  return 0;                                               // (a) statement
+}
+
+// (a) Identify the directives and statements in this program.
+
+// (b) What output does the program produce?
+//
+// Parkinson's Law:
+// Work expand so as to fill the time
+// available for its completion.
