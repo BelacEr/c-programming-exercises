3990c0b201a6c993f6226027fb5a5d1418dd363c
BelacEr <caleblopezmorgado2007@gmail.com>
2025-10-05 12:52:11 -0600

    Exercise 4-1: Write a program to print your name, social security number and date of birth

---
+++ practical-c-oualline/04_basic_declarations_and_expressions/programming_exercises/04_01.c
@@ -0,0 +1,13 @@
+/* Exercise 4-1: Write a program to print your name, social security number and date of birth */
+
+#include <stdio.h>
+
+int main(void)
+{
+    // Fake information, of course.
+    printf("Name: Lonnie J. Bell\n");
+    printf("Social security number: 128-82-XXXX\n");
+    printf("Date of birth: April 30, 2007\n");
+
+    return (0);
+}
